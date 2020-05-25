831ab09ae67ea001d812dee2d231cf75d83bd008
yamc <chen.yam@iongroup.com>
2020-05-25 16:57:01 -0400

    completed Dependency Inversion Overview Project

--- Section 2 - SOLID Design Principles/Dependency Inversion Principle/main.cpp
+++ Section 2 - SOLID Design Principles/Dependency Inversion Principle/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <tuple>
 
+// Dependency Inversion Principle
 // A. High-level modules should not depend on low-level modules. Both should depend on abstractions.
 // B. Abstractions should not depend on details. Details should depend on abstractions.
 
